a13fe252d64ed97e28f1108ba4b6e74c34e62d35
Brad Sickles <brad.sickles@gmail.com>
2022-08-18 12:23:38 -0400

    Use main module to configure lambda permission (#2)

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,14 @@ output "public_urls" {
     }
   ]
 }
+
+output "permissions" {
+  value = [
+    {
+      sid_prefix = "AllowGatewayAccess"
+      action     = "lambda:InvokeFunction"
+      principal  = "apigateway.amazonaws.com"
+      source_arn = "${aws_apigatewayv2_api.this.execution_arn}/*/${aws_apigatewayv2_api_mapping.this.stage}"
+    }
+  ]
+}
